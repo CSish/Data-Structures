29f5a3c1c1748f80674468bd1f9b3219f45042ae
tactnician <tactnician@gmail.com>
2019-12-17 08:48:03 -0500

    added move_to_end and move_to_front methods

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -89,17 +89,26 @@ class DoublyLinkedList:
         value = self.tail.value
         self.delete(self.tail)
         return value
-        
+
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new head node of the List."""
     def move_to_front(self, node):
-        pass
+        if node is self.head:
+            return
+        value = node.value()
+        self.delete(node)
+        self.add_to_head(value)
+
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List."""
     def move_to_end(self, node):
-        pass
+        if node is self.head: 
+            return
+        value = node.value
+        self.delete(node)
+        self.add_to_tail(value)
 
     """Removes a node from the list and handles cases where
     the node was the head or the tail"""
